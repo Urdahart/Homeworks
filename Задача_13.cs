5806ba79aeae38acada63856c37191ad94f272d6
Urdahart <120499431+Urdahart@users.noreply.github.com>
2022-12-21 20:32:56 +0300

    Задача_13

---
+++ Задача_13.cs
@@ -0,0 +1,14 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет 
+//32679 -> 6
+
+Console.WriteLine("Введите число");
+string a = Convert.ToString(Console.ReadLine());
+int length = a.Length;
+
+if (length>2)
+    {Console.WriteLine(a[2]);}
+else
+    {Console.WriteLine("Третьей цифры нет");}
+
