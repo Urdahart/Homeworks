f5f07da01f621fe759b287d79e40691e24c73f36
Urdahart <120499431+Urdahart@users.noreply.github.com>
2022-12-24 18:31:31 +0300

    Задача_27

---
+++ Задача_27.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число");
+string a = Console.ReadLine();
+
+int SumNumbers()
+{
+    int sum=0;
+
+    for (int i=0; i < a.Length; i++)
+    {
+        sum+=Convert.ToInt32(a[i].ToString());
+    }
+    return sum;
+} 
+
+Console.WriteLine(SumNumbers());
