fddc8417d8bad96945a869d0d9828f8ddcebd859
Urdahart <120499431+Urdahart@users.noreply.github.com>
2023-01-04 16:49:34 +0300

    Задача_66

---
+++ Задача_66.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите М");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N");
+int n=Convert.ToInt32(Console.ReadLine());
+
+if(m>n) Console.WriteLine("Числа введены некорректно, M не может быть меньше N");
+else
+    {
+        //создаем метод
+        int MethodSum(int start, int end)
+        {
+            if (start==end) return end;
+            return (start+MethodSum(start+1,end));
+        }
+        Console.WriteLine("Сумма элементов равна "+MethodSum(m,n));
+    }
